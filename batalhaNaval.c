e751280a248f575567332c940072f9e285376fe9
Iann Lucas Belle Monteiro <161357635+Iann-Monteiro@users.noreply.github.com>
2024-09-11 22:51:56 +0000

    Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-#define TAMANHO_TABULEIRO 5
+#define TAMANHO_TABULEIRO 10
 #define TAMANHO_NAVIO_VERTICAL 3
 #define TAMANHO_NAVIO_HORIZONTAL 4
+#define TAMANHO_NAVIO_DIAGONAL1 3
+#define TAMANHO_NAVIO_DIAGONAL2 4
 
 int main() {
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0};
@@ -10,25 +12,29 @@ int main() {
     int x_navio_vertical = 1;
     int y_navio_vertical = 0;
 
-    int x_navio_horizontal = 3;
+    int x_navio_horizontal = 5;
     int y_navio_horizontal = 2;
 
+    int x_navio_diagonal1 = 6;
+    int y_navio_diagonal1 = 5;
+
+    int x_navio_diagonal2 = 3;
+    int y_navio_diagonal2 = 7;
+
     for (int i = 0; i < TAMANHO_NAVIO_VERTICAL; i++) {
-        tabuleiro[y_navio_vertical + i][x_navio_vertical] = 1;
+        tabuleiro[y_navio_vertical + i][x_navio_vertical] = 3;
     }
 
     for (int i = 0; i < TAMANHO_NAVIO_HORIZONTAL; i++) {
-        tabuleiro[y_navio_horizontal][x_navio_horizontal + i] = 1;
+        tabuleiro[y_navio_horizontal][x_navio_horizontal + i] = 3;
     }
 
-    printf("Coordenadas do navio vertical (3 partes):\n");
-    for (int i = 0; i < TAMANHO_NAVIO_VERTICAL; i++) {
-        printf("Parte %d: (%d, %d)\n", i + 1, x_navio_vertical, y_navio_vertical + i);
+    for (int i = 0; i < TAMANHO_NAVIO_DIAGONAL1; i++) {
+        tabuleiro[y_navio_diagonal1 + i][x_navio_diagonal1 + i] = 3;
     }
 
-    printf("\nCoordenadas do navio horizontal (4 partes):\n");
-    for (int i = 0; i < TAMANHO_NAVIO_HORIZONTAL; i++) {
-        printf("Parte %d: (%d, %d)\n", i + 1, x_navio_horizontal + i, y_navio_horizontal);
+    for (int i = 0; i < TAMANHO_NAVIO_DIAGONAL2; i++) {
+        tabuleiro[y_navio_diagonal2 + i][x_navio_diagonal2 + i] = 3;
     }
 
     printf("\nTabuleiro:\n");
