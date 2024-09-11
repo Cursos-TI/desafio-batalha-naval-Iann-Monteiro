85deeebc344897da2f50794b6a895c4df6618bf8
Iann Lucas Belle Monteiro <161357635+Iann-Monteiro@users.noreply.github.com>
2024-09-11 23:13:16 +0000

    Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,49 +1,69 @@
 #include <stdio.h>
 
-#define TAMANHO_TABULEIRO 10
-#define TAMANHO_NAVIO_VERTICAL 3
-#define TAMANHO_NAVIO_HORIZONTAL 4
-#define TAMANHO_NAVIO_DIAGONAL1 3
-#define TAMANHO_NAVIO_DIAGONAL2 4
+#define TAMANHO_TABULEIRO 5
+
+void exibirMatriz(int matriz[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void habilidadeCone(int matriz[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int x, int y) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            if (i + j >= y && i - j <= x) {
+                matriz[i][j] = 1;
+            }
+        }
+    }
+}
+
+void habilidadeCruz(int matriz[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int x, int y) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        matriz[x][i] = 1;
+        matriz[i][y] = 1;
+    }
+}
+
+void habilidadeOctaedro(int matriz[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int x, int y) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            if (abs(i - x) + abs(j - y) <= 1) {
+                matriz[i][j] = 1;
+            }
+        }
+    }
+}
 
 int main() {
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0};
 
-    int x_navio_vertical = 1;
-    int y_navio_vertical = 0;
+    printf("Habilidade em cone:\n");
+    habilidadeCone(tabuleiro, 2, 2);
+    exibirMatriz(tabuleiro);
 
-    int x_navio_horizontal = 5;
-    int y_navio_horizontal = 2;
-
-    int x_navio_diagonal1 = 6;
-    int y_navio_diagonal1 = 5;
-
-    int x_navio_diagonal2 = 3;
-    int y_navio_diagonal2 = 7;
-
-    for (int i = 0; i < TAMANHO_NAVIO_VERTICAL; i++) {
-        tabuleiro[y_navio_vertical + i][x_navio_vertical] = 3;
-    }
-
-    for (int i = 0; i < TAMANHO_NAVIO_HORIZONTAL; i++) {
-        tabuleiro[y_navio_horizontal][x_navio_horizontal + i] = 3;
-    }
-
-    for (int i = 0; i < TAMANHO_NAVIO_DIAGONAL1; i++) {
-        tabuleiro[y_navio_diagonal1 + i][x_navio_diagonal1 + i] = 3;
-    }
-
-    for (int i = 0; i < TAMANHO_NAVIO_DIAGONAL2; i++) {
-        tabuleiro[y_navio_diagonal2 + i][x_navio_diagonal2 + i] = 3;
-    }
-
-    printf("\nTabuleiro:\n");
     for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
         for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
-            printf("%d ", tabuleiro[i][j]);
+            tabuleiro[i][j] = 0;
         }
-        printf("\n");
     }
+
+    printf("\nHabilidade em cruz:\n");
+    habilidadeCruz(tabuleiro, 2, 2);
+    exibirMatriz(tabuleiro);
+
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    printf("\nHabilidade em octaedro:\n");
+    habilidadeOctaedro(tabuleiro, 2, 2);
+    exibirMatriz(tabuleiro);
 
     return 0;
 }
